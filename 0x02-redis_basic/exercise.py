5963aad2e6bde3aa7f661b4916679f41ca405002
root <mohammed2004koki@gmail.com>
2024-10-24 04:10:36 +0300

    incr

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,22 @@ Redis basic.
 from typing import Union, Callable, Optional
 import redis
 import uuid
+from functools import wraps
+
+
+def count_calls(funct: Callable) -> Callable:
+    """
+    Function decorator to count the number of times a function is called.
+    """
+    key = funct.__qualname__
+    @wraps(funct)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        self._redis.incr(key)
+        return funct(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -18,17 +34,20 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
     def get(self, key: str, fn: Optional[Callable] = None) -> str:
         """
         GET FUNCTION
         """
         return self._redis.get(key) if fn is None else fn(self._redis.get(key))
+
     def get_str(self, data: str) -> str:
         return (data).decode('utf-8')
+
     def get_int(self, data: str) -> int:
         return int(data)
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -8,15 +8,9 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
 
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    call = cache.get_str
-    strng = cache.get(key, call)
-    print(strng)
-
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
