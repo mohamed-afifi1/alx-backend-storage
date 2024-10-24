80ec9b56a8f8fd42fb2552b4eb209cf8dff5b194
root <mohammed2004koki@gmail.com>
2024-10-24 04:16:21 +0300

    history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -59,6 +59,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
